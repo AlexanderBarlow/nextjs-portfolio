7f527a2c101b26a0bad30333e1cb8b1f4fffe6d5
alex barlow <alex.barlow@rocketmail.com>
2023-07-04 12:44:53 -0400

    change

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+
+// const withPWA = require('next-pwa');
 
  
-module.exports = withPWA({
-  // here goes your Next.js configuration
-});
+
  
-const nextConfig = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   disable: process.env.NODE_ENV ===      'development',
   skipWaiting: true
 })
  
-module.exports = nextConfig;
 
+module.exports = withPWA({
+  // here goes your Next.js configuration
+});
 
 
