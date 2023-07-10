8679a50eadf850b5cbb8b08d1f520cfabdb2bc89
alex barlow <alex.barlow@rocketmail.com>
2023-07-10 19:06:11 -0400

    mobile response

--- components/Header.js
+++ components/Header.js
@@ -17,25 +17,12 @@ const navigation = [
 
 function Header() {
   return (
-    
-    <Disclosure as="nav" className="bg-blue-950">
+    <Disclosure as="nav" className="trasparent sm:justify-center sm:place-content-center">
     {({ open }) => (
       <>
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/* Mobile menu button*/}
-              <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                <span className="sr-only">Open main menu</span>
-                {open ? (
-                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                ) : (
-                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                )}
-              </Disclosure.Button>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="hidden sm:ml-6 sm:block">
+            <div className="flex flex-1 items-center justify-center">
+              <div className="">
                 <div className="flex space-x-4">
                   {navigation.map((item) => (
                     <a
@@ -53,7 +40,7 @@ function Header() {
                 </div>
               </div>
             </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2">
               <button
                 type="button"
                 className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -63,7 +50,7 @@ function Header() {
               </button>
             </div>
           </div>
-        </div>
+        
       </>
     )}
   </Disclosure>
