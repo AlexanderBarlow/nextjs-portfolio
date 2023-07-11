635cc4e9a1bfe275496280860ac8d3015408e09a
alex barlow <alex.barlow@rocketmail.com>
2023-07-11 14:03:59 -0400

    styling

--- components/Projects.js
+++ components/Projects.js
@@ -8,39 +8,23 @@ const projects = [
     name: "Password Tracker",
     description: "A password tracker.",
     deployment: "https://password-tracker.herokuapp.com/login",
-    repo: "",
+    repo: "https://github.com/AlexanderBarlow/password-tracker",
     skill1: "ReactJS",
     skill2: "NodeJS",
   },
   {
-    name: "Disney",
+    name: "Disney Charactor Locator",
     description: "A character locator.",
-    deployment: "https://password-tracker.herokuapp.com/login",
-    repo: "",
+    deployment: "https://alexanderbarlow.github.io/disney-character-locator/",
+    repo: "https://github.com/AlexanderBarlow/disney-character-locator",
     skill1: "Handlebars",
     skill2: "NodeJS",
   },
   {
     name: "NestEase",
     description: "A rental site.",
-    deployment: "https://password-tracker.herokuapp.com/login",
-    repo: "",
-    skill1: "ReactJS",
-    skill2: "NodeJS",
-  },
-  {
-    name: "NestEase",
-    description: "A rental site.",
-    deployment: "https://password-tracker.herokuapp.com/login",
-    repo: "",
-    skill1: "ReactJS",
-    skill2: "NodeJS",
-  },
-  {
-    name: "NestEase",
-    description: "A rental site.",
-    deployment: "https://password-tracker.herokuapp.com/login",
-    repo: "",
+    deployment: "https://nestease.herokuapp.com/",
+    repo: "https://github.com/AlexanderBarlow/rental-site",
     skill1: "ReactJS",
     skill2: "NodeJS",
   },
@@ -57,29 +41,30 @@ function Projects() {
       id="projects"
     >
       <a href="projects"></a>
-      <div className="w-60 row-start-1 row-span-1" id="lottie1">
-        <h1 className="text-white text-4xl underline">Projects</h1>
+      <div className="lg:w-60 lg:row-start-1 lg:row-span-1 row start-1 row-span-1" id="lottie1">
+        <h1 className="text-white text-6xl underline">Projects</h1>
       </div>
-      <div className="row-end-4 row-span-3">
+      <div className="lg:row-end-4 lg:row-span-3">
         <div className="flex space-x-4">
           {projects.map((item) => (
             <div
               className={classNames(
-                "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "rounded-md px-3 py-2 text-sm font-medium "
+                "text-gray-300 lg:hover:bg-gray-700 hover:text-white",
+                "lg:rounded-md lg:px-8 lg:py-8 lg:text-sm lg:font-medium ",
+                ""
               )}
-            >
-              <h1 key={item.name} className="text-purple-700">
+            > <a href={item.deployment}>
+              <h1 key={item.name} className="text-purple-700 text-3xl">
                 {item.name}
               </h1>
-              <p>{item.description}</p>
-              <ul>
-                <li>{item.skill1}</li>
-                <li>{item.skill2}</li>
+              <p className="lg:text-xl">{item.description}</p>
+              <ul className="">
+                <li className="bg-purple-950 text-purple-400 font-semibold  lg:py-2 lg:px-4 py-1 px-1 border border-purple-500  rounded ">{item.skill1}</li>
+                <li className="bg-purple-950  text-purple-400 font-semibold  lg:py-2 lg:px-4 py-1 px-1 border border-purple-500  rounded">{item.skill2}</li>
               </ul>
-              <button href={item.deployment}>Deployment</button>
-              <button href={item.repo}>Repository</button>
+              </a>
             </div>
+            
           ))}
         </div>
       </div>
